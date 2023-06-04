614fa828ae50e1e357b2cad9b73441d19df79744
Earl Bit <earlbit247@gmail.com>
2023-06-04 23:06:04 +0200

    JS10

---
+++ main.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+let num2 = 5;
+
+function findLargerNumber(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+let result = findLargerNumber(num1, num2);
+console.log("The larger number is: " + result);
